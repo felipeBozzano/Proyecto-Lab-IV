9f878db0d6a74774eb4abf62e24de73307e2c9a4
franciscolmos <francisco.olmos.ubp@gmail.com>
2021-12-04 18:12:48 -0300

    settings for document

--- myubp/myubp/settings.py
+++ myubp/myubp/settings.py
@@ -139,3 +139,5 @@ AUTH_USER_MODEL = 'users.UserProfile'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'}
